aab69f11337aee2bf458f5938bdffa9f7cec3f7c
mostafashraf18 <ma2963629@gmail.com>
2023-10-21 10:43:58 +0300

    F

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def to_uper(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return (ord(c) - 32)
-    else:
-        return ord(c)
+# Author - Godswill Kalu
 
-
-def uppercase(string):
-    string_new = ""
-    for c in string:
-        string_new += "%c" % to_uper(c)
-    print("{:s}".format(string_new))
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
